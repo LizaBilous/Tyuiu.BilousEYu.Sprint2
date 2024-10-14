4d7ca8a4f9fa8a01eda3c4d990af1e2b59753f31
Bilous.liza@bk.ru <Bilous.liza@bk.ru>
2024-10-14 19:02:28 +0500

    Task2

---
+++ Tyuiu.BilousEYu.Sprint2.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.BilousEYu.Sprint2.Task2.V19.Lib;
+namespace Tyuiu.BilousEYu.Sprint2.Task2.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            int y = 3;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BilousEYu.Sprint2.Task2.V19/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.BilousEYu.Sprint2.Task2.V19.Lib;
+namespace Tyuiu.BilousEYu.Sprint2.Task2.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Спринт #2 | Выполнила:  Билоус Е.Ю.| ИСТНб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* спринт #2                                                               *");
+            Console.WriteLine("* Тема : Оператор if – полная и короткая форма записи                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнила: Билоус Елизавета Юрьевна | ИСТНб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на, которая запрашивает целые значения с клавиатуры  *");
+            Console.WriteLine("*и вычисляет находится ли точка с координатами X,Y                        *");
+            Console.WriteLine("* в заштрихованной области.                                               *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
