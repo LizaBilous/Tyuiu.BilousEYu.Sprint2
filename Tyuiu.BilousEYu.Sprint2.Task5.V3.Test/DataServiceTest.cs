16ea22e55d68b6563cbf0437921f5902646c3661
Bilous.liza@bk.ru <Bilous.liza@bk.ru>
2024-10-15 16:07:41 +0500

    Task5

---
+++ Tyuiu.BilousEYu.Sprint2.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+using Tyuiu.BilousEYu.Sprint2.Task5.V3.Lib;
+namespace Tyuiu.BilousEYu.Sprint2.Task5.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDayName()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("понедельник", ds.FindDayName(1));
+            Assert.AreEqual("вторник", ds.FindDayName(2));
+            Assert.AreEqual("среда", ds.FindDayName(3));
+            Assert.AreEqual("четверг", ds.FindDayName(4));
+            Assert.AreEqual("пятница", ds.FindDayName(5));
+            Assert.AreEqual("суббота", ds.FindDayName(6));
+            Assert.AreEqual("воскресенье", ds.FindDayName(7));
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindDayName(-1);
+            });
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindDayName(8);
+            });
+        }
+    }
+}
---
+++ Tyuiu.BilousEYu.Sprint2.Task5.V3/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.BilousEYu.Sprint2.Task5.V3.Lib;
+namespace Tyuiu.BilousEYu.Sprint2.Task5.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #2 | Выполнила: Билоус Е.Ю. | ИСТНб-24-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спритн #2                                                              *");
+            Console.WriteLine("* Тема: Оператор switch                                                  *");
+            Console.WriteLine("* Задание #5                                                             *");
+            Console.WriteLine("* Вариант #3                                                             *");
+            Console.WriteLine("* Выполнила: Билоус Елизавета Юрьевна | ИСТНб-24-1                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("Введите номер дня :");
+            int numDay = Convert.ToInt32(Console.ReadLine());
+            string res;
+
+            if ((numDay < 1) || (numDay > 7))
+            {
+                res = "Введенно неверное значение";
+            }
+            else
+            {
+                res = "Это день " + ds.FindDayName(numDay);
+            }
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
